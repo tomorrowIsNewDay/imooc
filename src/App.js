e15b14beec2c8269fc1a328e8dac0c128220d2de
lee_ming <602387427@qq.com>
2019-10-22 00:16:06 +0800

    :fire: antd-mobile

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button } from 'antd-mobile'
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,18 +7,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <Button type='primary'>learn react</Button>
+        <Button type='primary'>bingo</Button>
       </header>
     </div>
   );
