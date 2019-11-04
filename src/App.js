4c1ec8baeb2b4991eab9173ad648e4487f3bc53f
lee_ming <602387427@qq.com>
2019-11-04 23:52:50 +0800

    SS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import { Provider } from 'react-redux' 
 import store from './store'
 
 import './App.css';
 
 import { Dashboard, Login } from './view'
+
+// function NotFound(props){
+//   return (
+//     <h2>NotFound {props.match.params.location}</h2>
+//   )
+// }
+function NotFound1(){
+  return (
+    <h2>NotFound </h2>
+  )
+}
 
 function App() {
   return (
@@ -14,7 +25,9 @@ function App() {
         <BrowserRouter>
           <Switch>
             <Route path='/login' component={Login}/>
-            <Route component={Dashboard}/>
+            <Route path='/dashboard' component={Dashboard}/>
+            {/* <Route path='/:location' component={NotFound}/> */}
+            <Redirect to='/404' component={NotFound1}/>
           </Switch>
         </BrowserRouter>
       </Provider>
